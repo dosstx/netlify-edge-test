16f8cc85132da1432645dbdd61af6088fa746d35
Nick <nickm@ogmda.com>
2023-05-26 13:05:02 -0500

    first commit

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <button type="button" @click="apiTest">API request</button>
+    {{ hello }}
+  </div>
+</template>
+
+<script setup>
+const hello = ref('')
+const apiTest = async () => {
+  const { data } = await useFetch('api/hello')
+  hello.value = data.value
+}
+</script>
