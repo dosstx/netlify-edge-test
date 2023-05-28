7b02eb981bff5d572f875cbf3b62a34ccc220259
Nick <nickm@ogmda.com>
2023-05-28 14:20:52 -0500

    test on regular netlify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     nitro: {
-        preset: 'netlify-edge'
+        preset: 'netlify'
     },
     runtimeConfig: {
         OPENAI_API_KEY: ""
