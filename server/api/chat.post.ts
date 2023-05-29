314b601d4b748a327ef4632c8ab9b9481a18ea8f
Nick <nickm@ogmda.com>
2023-05-29 06:32:45 -0500

    back to beginning for now

--- server/api/chat.post.ts
+++ server/api/chat.post.ts
@@ -1,24 +1,11 @@
-import * as openai from "https://deno.land/x/openai@1.3.4/mod.ts";
-
-const config = useRuntimeConfig()
-
-const openAI = new openai(Deno.env.get(config.OPENAI_API_KEY))
-
+import { getChatStream } from '../utils/ai'
 export default defineEventHandler(async (event) => {
-
-  const chatCompletion = await openAI.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      { "role": "system", "content": "You are a helpful assistant." },
-      { "role": "user", "content": "Who won the world series in 2020?" },
-      {
-        "role": "assistant",
-        "content": "The Los Angeles Dodgers won the World Series in 2020.",
-      },
-      { "role": "user", "content": "Where was it played?" },
-    ],
-  });
-
-  console.log(chatCompletion);
+  try {
+    const { messages } = await readBody(event)
+    const stream = await getChatStream({ messages })
+    return stream
+  } catch (error) {
+    console.error('error on server', error)
+  }
 
 })
